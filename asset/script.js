cb2d92bbee9d813af199bd6e7cc19ee0c56f2a7f
Rafisakhi <68719207+Rafisakhi@users.noreply.github.com>
2022-08-10 13:51:02 +0700

    Add files via upload

--- asset/script.js
+++ asset/script.js
@@ -5,3 +5,11 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navBar.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    a;
+  })
+);
